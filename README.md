3144fd4d129ea423da0af9939a136ee1b6a6b862
cjbrock <corinna@flatironschool.com>
2018-06-07 10:44:37 -0400

    Add dog walking theme to lab. Also remove legacy real estate files, as the contact form didn't actually contain any helpful info.

--- README.md
+++ README.md
@@ -2,18 +2,19 @@
 
 ## Problem Statement
 
-Business of all types and sizes want to provide multiple ways for their potential
-customers to contact them.  One of the most common ways to do this is on a website is to provide
-a _contact form_.
+So far, you've developed a pretty solid neighborhood dog walking business, but
+you'd love to be able to have a better internet presence and to gather more
+clients to build your empire. One of the most common ways to do this is on a
+website is to provide a _contact form_.
 
 In this lab, we will practice what we've learned about HTML forms by creating a
 contact form similar to those we see on business websites.
 
 ## Objectives
 
-1. Apply what we know of HTML forms to build a functional a contact form
+1. Apply what we know of HTML forms to build a contact form
 
-## Reinforcing Our Understanding of HTML Forms
+## Apply What We Know of HTML Forms to Build a Contact Form
 
 To see and test your form in action, run `httpserver` or open `index.html` in a
 new browser tab. To test your work, run `learn`.
@@ -22,25 +23,27 @@ Some basic HTML has been provided for this lab, along with HTML comments on what
 is needed.  Follow the provided comments and test messages to see what is needed
 when creating the form.  At completion, your form should have:
 
-* A _required_ text input for a full name with a placeholder, "Enter Name"
-* A _required_ email type input for an email address with a placeholder, "Enter Email"
-* A _tel_ type input for a telephone number with a placeholder, "Enter Telephone (optional)"
-* A text area for a user to include a message with a placeholder, "Enter Message"
+* A _required_ text input for a full name with a placeholder, "Owner Name"
+* A _required_ email type input for an email address with a placeholder, "Owner Email"
+* A _tel_ type input for a telephone number with a placeholder, "Owner  Telephone (optional)"
+* A text area for a user to include a message with a placeholder, "Dog notes"
 * Labels for each text input
 * A checkbox with text of your choosing
 * A submit button
 
 ### The `required` Attribute
 
-The `required` attribute prevents the browser from submitting a form.  All
-required inputs must be filled in properly, preventing an unneeded page reload.
-This functionality is baked into HTML5 for us automatically. All that is
-needed is to include the word `required` as an attribute, without setting it to
-any value.
+Generally, there are certain things that each websites determine is their "must
+have" data. For your dog walking business, this information is the Owner Name,
+and Owner Email. The `required` attribute prevents the browser from submitting a
+form.  All required inputs must be filled in properly, preventing an unneeded
+page reload. Luckily for us, This functionality is baked into HTML5 for us
+automatically. All that is needed is to include the word `required` as an
+attribute, without setting it to any value.
 
 When an input is required, the `type` attribute (e.g., `text` vs `email`) will
-determine what the browser checks. In the case of an email, for instance, the input must
-match the format of an email address, including the "@".
+determine what the browser checks. In the case of an email, for instance, the
+input must match the format of an email address, including the "@".
 
 ## Conclusion
 
@@ -48,8 +51,9 @@ Forms are an essential way for users to submit data to a website. Some forms,
 like search bars, only contain a single input field, but we can make them as
 complex as we need.
 
-We are only focused on building out the front end side of this form.  When a
-form is submitted, it is up to the _backend_ of a website to decide how to
-handle and store form data when submitted.
+Right now, we are only focused on building out the front end side of this form.
+When a form is submitted, it is up to the _backend_ of a website to decide how
+to handle and store form data when submitted. Now you've got everything you need
+to keep in contact with current clients, and connect with new ones!
 
 <p class='util--hide'>View <a href='https://learn.co/lessons/html-map-contact-form-code-along'>HTML Map Contact Form Code-along</a> on Learn.co and start learning to code for free.</p>
--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -8,47 +8,47 @@ RSpec.describe 'index.html' do
   end
 
   context 'within <form>' do
-    it 'contains a "text" type <input> tag with an id of "fullname" and placeholder equal to "Enter Name" ' do
+    it 'contains a "text" type <input> tag with an id of "ownername" and placeholder equal to "Owner Name" ' do
       input = parsed_html.search('input')[0]
       expect(input).to_not be_nil, "No <input> tags were found"
       expect(input.attributes["type"]).to_not be_nil, "No type attribute was found on the first 'input' tag"
       expect(input.attributes["type"]).to match(/text/) , "The first input should have an type set to 'text'"
       expect(input.attributes["id"]).to_not be_nil, "No id attribute was found on the first 'input' tag"
-      expect(input.attributes["id"]).to match(/fullname/) , "The first input should have an id set to 'fullname'"
+      expect(input.attributes["id"]).to match(/ownername/) , "The first input should have an id set to 'ownername'"
       expect(input.attributes["placeholder"]).to_not be_nil, "No placeholder attribute was found on the first 'input' tag"
-      expect(input.attributes["placeholder"]).to match(/Enter Name/) , "The first input should have a placeholder set to 'Enter Name'"
+      expect(input.attributes["placeholder"]).to match(/Owner Name/) , "The first input should have a placeholder set to 'Owner Name'"
     end
 
-    it 'contains an "email" type <input> tag with an id of "email" and placeholder equal to "Enter Name" ' do
+    it 'contains an "email" type <input> tag with an id of "email" and placeholder equal to "Owner Name" ' do
       input = parsed_html.search('input')[1]
       expect(input.attributes["type"]).to_not be_nil, "No type attribute was found on the second 'input' tag"
       expect(input.attributes["type"]).to match(/email/) , "The second input should have an type set to 'email'"
       expect(input.attributes["id"]).to_not be_nil, "No id attribute was found on the second 'input' tag"
       expect(input.attributes["id"]).to match(/email/) , "The second input should have an id set to 'email'"
       expect(input.attributes["placeholder"]).to_not be_nil, "No placeholder attribute was found on the second 'input' tag"
-      expect(input.attributes["placeholder"]).to match(/Enter Email/) , "The second input should have a placeholder set to 'Enter Email'"
+      expect(input.attributes["placeholder"]).to match(/Owner Email/) , "The second input should have a placeholder set to 'Owner Email'"
     end
 
-    it 'includes the "required" attribute on the fullname and email input tags' do
+    it 'includes the "required" attribute on the ownername and email input tags' do
       inputs = parsed_html.search('input')
-      expect(inputs[0].attributes["required"]).to_not be_nil, "Add the 'required' attribute to the fullname input tag"
+      expect(inputs[0].attributes["required"]).to_not be_nil, "Add the 'required' attribute to the ownername input tag"
       expect(inputs[1].attributes["required"]).to_not be_nil, "Add the 'required' attribute to the email input tag"
     end
 
-    it 'contains an "tel" type <input> tag with an id of "phone" and placeholder equal to "Enter Telephone (optional)" ' do
+    it 'contains an "tel" type <input> tag with an id of "phone" and placeholder equal to "Owner Telephone (optional)" ' do
       input = parsed_html.search('input')[2]
       expect(input.attributes["type"]).to_not be_nil, "No type attribute was found on the third 'input' tag"
       expect(input.attributes["type"]).to match(/tel/) , "The third input should have an type set to 'tel'"
       expect(input.attributes["id"]).to_not be_nil, "No id attribute was found on the third 'input' tag"
       expect(input.attributes["id"]).to match(/phone/) , "The third input should have an id set to 'phone'"
       expect(input.attributes["placeholder"]).to_not be_nil, "No placeholder attribute was found on the third 'input' tag"
-      expect(input.attributes["placeholder"].value).to eq("Enter Telephone (optional)") , "The third input should have a placeholder set to 'Enter Telephone (optional)'"
+      expect(input.attributes["placeholder"].value).to eq("Owner Telephone (optional)") , "The third input should have a placeholder set to 'Owner Telephone (optional)'"
     end
 
-    it 'contains <label> tags for the fullname, email, phone and message form inputs ' do
+    it 'contains <label> tags for the ownername, email, phone and message form inputs ' do
       labels = parsed_html.search('label')
       expect(labels[0].attributes["for"]).to_not be_nil, "No label tags found"
-      expect(labels[0].attributes["for"].value).to eq("fullname"), "Include a label with a 'for' attribute set to 'fullname'"
+      expect(labels[0].attributes["for"].value).to eq("ownername"), "Include a label with a 'for' attribute set to 'ownername'"
       expect(labels[1].attributes["for"]).to_not be_nil, "No label tag for email input found"
       expect(labels[1].attributes["for"].value).to eq("email"), "Include a label with a 'for' attribute set to 'email'"
       expect(labels[2].attributes["for"]).to_not be_nil, "No label tag for phone input found"
@@ -57,12 +57,12 @@ RSpec.describe 'index.html' do
       expect(labels[3].attributes["for"].value).to eq("message"), "Include a label with a 'for' attribute set to 'message'"
     end
 
-    it 'contains an <textarea> tag with an id of "message" and placeholder equal to "Enter Message" ' do
+    it 'contains an <textarea> tag with an id of "message" and placeholder equal to "Dog notes" ' do
       input = parsed_html.search('textarea')[0]
       expect(input.attributes["id"]).to_not be_nil, "No id attribute was found on the 'textarea' tag"
       expect(input.attributes["id"]).to match(/message/) , "The textarea tag should have an id set to 'message'"
       expect(input.attributes["placeholder"]).to_not be_nil, "No placeholder attribute was found on the 'textarea' tag"
-      expect(input.attributes["placeholder"]).to match(/Enter Message/) , "The third input should have a placeholder set to 'Enter Message'"
+      expect(input.attributes["placeholder"]).to match(/Dog notes/) , "The third input should have a placeholder set to 'Dog notes'"
     end
 
     it 'contains a checkbox' do
