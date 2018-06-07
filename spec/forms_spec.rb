2c7665dbb810db565cc2f5cc9e75cf38c6a2dc07
cjbrock <corinna@flatironschool.com>
2018-06-07 11:22:22 -0400

    fix email typo

--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe 'index.html' do
       expect(input.attributes["placeholder"]).to match(/Owner Name/) , "The first input should have a placeholder set to 'Owner Name'"
     end
 
-    it 'contains an "email" type <input> tag with an id of "email" and placeholder equal to "Owner Name" ' do
+    it 'contains an "email" type <input> tag with an id of "email" and placeholder equal to "Owner Email" ' do
       input = parsed_html.search('input')[1]
       expect(input.attributes["type"]).to_not be_nil, "No type attribute was found on the second 'input' tag"
       expect(input.attributes["type"]).to match(/email/) , "The second input should have an type set to 'email'"
@@ -45,6 +45,14 @@ RSpec.describe 'index.html' do
       expect(input.attributes["placeholder"].value).to eq("Owner Telephone (optional)") , "The third input should have a placeholder set to 'Owner Telephone (optional)'"
     end
 
+    it 'contains an <textarea> tag with an id of "message" and placeholder equal to "Dog notes" ' do
+      input = parsed_html.search('textarea')[0]
+      expect(input.attributes["id"]).to_not be_nil, "No id attribute was found on the 'textarea' tag"
+      expect(input.attributes["id"]).to match(/message/) , "The textarea tag should have an id set to 'message'"
+      expect(input.attributes["placeholder"]).to_not be_nil, "No placeholder attribute was found on the 'textarea' tag"
+      expect(input.attributes["placeholder"]).to match(/Dog notes/) , "The third input should have a placeholder set to 'Dog notes'"
+    end
+    
     it 'contains <label> tags for the ownername, email, phone and message form inputs ' do
       labels = parsed_html.search('label')
       expect(labels[0].attributes["for"]).to_not be_nil, "No label tags found"
@@ -55,14 +63,6 @@ RSpec.describe 'index.html' do
       expect(labels[2].attributes["for"].value).to eq("phone"), "Include a label with a 'for' attribute set to 'phone'"
       expect(labels[3].attributes["for"]).to_not be_nil, "No label tag for message input found"
       expect(labels[3].attributes["for"].value).to eq("message"), "Include a label with a 'for' attribute set to 'message'"
-    end
-
-    it 'contains an <textarea> tag with an id of "message" and placeholder equal to "Dog notes" ' do
-      input = parsed_html.search('textarea')[0]
-      expect(input.attributes["id"]).to_not be_nil, "No id attribute was found on the 'textarea' tag"
-      expect(input.attributes["id"]).to match(/message/) , "The textarea tag should have an id set to 'message'"
-      expect(input.attributes["placeholder"]).to_not be_nil, "No placeholder attribute was found on the 'textarea' tag"
-      expect(input.attributes["placeholder"]).to match(/Dog notes/) , "The third input should have a placeholder set to 'Dog notes'"
     end
 
     it 'contains a checkbox' do
